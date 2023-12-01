aa061f4da55ce99887ede6a994ed76545af2eac6
Gribovod <gribovod1@gmail.com>
2023-12-01 11:56:12 +0300

    Add solution 2023 Day 1

---
+++ AOC2023/Day1.cs
@@ -0,0 +1,90 @@
+﻿using AnyThings;
+
+namespace AOC2023
+{
+    internal class Day1 : DayPattern<List<string>>
+    {
+        public override void Parse(string singleText)
+        {
+            data = singleText.Split(Environment.NewLine).ToList();
+        }
+
+        (int, int, int, int) GetNumber(string input)
+        {
+            int last = -1;
+            int first = -1;
+            int lastIndex = -1;
+            int firstIndex = int.MaxValue;
+            string[] numbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            for (int n = 0; n < numbers.Length; n++)
+            {
+                int index = input.IndexOf(numbers[n]);
+                if (index >= 0 && index < firstIndex)
+                {
+                    first = n + 1;
+                    firstIndex = index;
+                }
+                index = input.LastIndexOf(numbers[n]);
+                if (index >= 0 && index > lastIndex)
+                {
+                    last = n + 1;
+                    lastIndex = index;
+                }
+            }
+            return (first, last, firstIndex, lastIndex);
+        }
+
+        public override string PartOne()
+        {
+            int result = 0;
+            for (int i = 0; i < data.Count; ++i)
+            {
+                string s = data[i];
+                int first = 0;
+                int last = 0;
+                for (int j = 0; j < s.Length; ++j)
+                {
+                    if (char.IsDigit(s[j]))
+                    {
+                        last = s[j] - '0';
+                        if (first == 0)
+                            first = last;
+
+                    }
+                }
+                result += first * 10 + last;
+            }
+            return result.ToString();
+        }
+
+        public override string PartTwo()
+        {
+            int result = 0;
+            for (int i = 0; i < data.Count; ++i)
+            {
+                string s = data[i];
+                (int firstText, int lastText, int firstTextIndex, int lastTextIndex) = GetNumber(s);
+                int first = firstText;
+                int last = lastText;
+                for (int j = 0; j < s.Length; ++j)
+                {
+                    if (char.IsDigit(s[j]))
+                    {
+                        if (j < firstTextIndex)
+                        {
+                            first = s[j] - '0';
+                            firstTextIndex = j;
+                        }
+                        if (j > lastTextIndex)
+                        {
+                            last = s[j] - '0';
+                            lastTextIndex = j;
+                        }
+                    }
+                }
+                result += first * 10 + last;
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ AOC2023/Program.cs
@@ -0,0 +1,4 @@
+﻿using CurrentDay = AOC2023.Day1;
+
+var currentDay = new CurrentDay();
+currentDay.Exec($"Data\\{currentDay.GetType().Name}.txt");
