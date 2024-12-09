ce1ab662c099fe76fb36fc07a044dd48dd829f09
Gribovod <gribovod1@gmail.com>
2024-12-09 08:07:34 +0300

    Add Day09

---
+++ AOC2024/Day09.cs
@@ -0,0 +1,24 @@
+﻿using AnyThings;
+
+namespace AOC2024
+{
+    internal class Day09 : DayPattern<string[]>
+    {
+        public override void Parse(string singleText)
+        {
+            data = singleText.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public override string PartOne()
+        {
+            long result = 0;
+            return result.ToString();
+        }
+
+        public override string PartTwo()
+        {
+            long result = 0;
+            return result.ToString();
+        }
+    }
+}
--- AOC2024/Program.cs
+++ AOC2024/Program.cs
@@ -1,4 +1,4 @@
-﻿using CurrentDay = AOC2024.Day07;
+﻿using CurrentDay = AOC2024.Day09;
 
 var currentDay = new CurrentDay();
 currentDay.Exec($"Data/{currentDay.GetType().Name}.txt");
