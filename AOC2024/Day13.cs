b12a8025d1687dcf84d65f13eeb960519c410f2f
Gribovod <gribovod1@gmail.com>
2024-12-12 11:37:47 +0300

    2024 Day13 Blank

---
+++ AOC2024/Day13.cs
@@ -0,0 +1,30 @@
+﻿using AnyThings;
+using Coord = (int r, int c);
+
+namespace AOC2024
+{
+    internal class Day13 : DayPattern<(char[][] map, HashSet<(int l, int c)> read)>
+    {
+        public override void Parse(string singleText)
+        {
+            string[] lines = singleText.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            data.map = new char[lines.Length][];
+            for (int l = 0; l < lines.Length; l++)
+            {
+                data.map[l] = lines[l].ToCharArray();
+            }
+            data.read = new();
+        }
+
+        public override string PartOne()
+        {
+            long result = 0;
+            return result.ToString();
+        }
+        public override string PartTwo()
+        {
+            long result = 0;
+            return result.ToString();
+        }
+    }
+}
--- AOC2024/Program.cs
+++ AOC2024/Program.cs
@@ -1,4 +1,4 @@
-﻿using CurrentDay = AOC2024.Day11;
+﻿using CurrentDay = AOC2024.Day13;
 
 var currentDay = new CurrentDay();
 currentDay.Exec($"Data/{currentDay.GetType().Name}.txt");
