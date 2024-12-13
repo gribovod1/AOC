443227ffbf020677be9593b1c4333fcd1d002ef7
Gribovod <gribovod1@gmail.com>
2024-12-13 14:29:49 +0300

    Solved 2024 Day13

--- AOC2024/Day13.cs
+++ AOC2024/Day13.cs
@@ -1,30 +1,55 @@
 ﻿using AnyThings;
-using Coord = (int r, int c);
+using Coord = (long x, long y);
 
 namespace AOC2024
 {
-    internal class Day13 : DayPattern<(char[][] map, HashSet<(int l, int c)> read)>
+    internal class Day13 : DayPattern<List<(Coord A, Coord B, Coord P)>>
     {
         public override void Parse(string singleText)
         {
-            string[] lines = singleText.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-            data.map = new char[lines.Length][];
-            for (int l = 0; l < lines.Length; l++)
+            string[] machines = singleText.Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            data = new();
+            for (int l = 0; l < machines.Length; l++)
             {
-                data.map[l] = lines[l].ToCharArray();
+                var pText = machines[l].Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+                var Atext = pText[0].Split(new char[] { ' ', ',', '+' }, StringSplitOptions.RemoveEmptyEntries);
+                var Btext = pText[1].Split(new char[] { ' ', ',', '+' }, StringSplitOptions.RemoveEmptyEntries);
+                var Ptext = pText[2].Split(new char[] { ' ', ',', '=' }, StringSplitOptions.RemoveEmptyEntries);
+                data.Add(((long.Parse(Atext[3]), long.Parse(Atext[5])),
+                    (long.Parse(Btext[3]), long.Parse(Btext[5])),
+                    (long.Parse(Ptext[2]), long.Parse(Ptext[4]))));
             }
-            data.read = new();
         }
 
         public override string PartOne()
         {
             long result = 0;
+            for (int l = 0; l < data.Count; ++l)
+                result += Calc(data[l]);
             return result.ToString();
         }
+
         public override string PartTwo()
         {
+
             long result = 0;
+            for (int l = 0; l < data.Count; ++l)
+            {
+                var m = data[l];
+                m.P.x += 10000000000000;
+                m.P.y += 10000000000000;
+                result += Calc(m);
+            }
             return result.ToString();
+        }
+
+        long Calc((Coord A, Coord B, Coord P) m)
+        {
+            if ((m.P.y * m.A.x - m.A.y * m.P.x) % (m.A.x * m.B.y - m.A.y * m.B.x) != 0) return 0;
+            var b = (m.P.y * m.A.x - m.A.y * m.P.x) / (m.A.x * m.B.y - m.A.y * m.B.x);
+            if ((m.P.x - m.B.x * b) % m.A.x != 0) return 0;
+            var a = (m.P.x - m.B.x * b) / m.A.x;
+            return b + a * 3;
         }
     }
 }
