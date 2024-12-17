4647e222db1eeddf2ad025d6101a39df28b7994f
Gribovod <gribovod1@gmail.com>
2024-12-17 16:55:58 +0300

    2024 Day 17 part two solved

--- AOC2024/Day17.cs
+++ AOC2024/Day17.cs
@@ -15,7 +15,7 @@ namespace AOC2024
             data.A = int.Parse(rText[0].Split(' ')[2]);
             data.B = int.Parse(rText[1].Split(' ')[2]);
             data.C = int.Parse(rText[2].Split(' ')[2]);
-            data.ProgramText = RegistersAndProgram[1].Remove(0,9);
+            data.ProgramText = RegistersAndProgram[1].Remove(0, 9);
             var program = data.ProgramText.Split(new string[] { "Program: ", "," }, StringSplitOptions.RemoveEmptyEntries);
             for (int r = 0; r < program.Length; r++)
                 data.Program.Add(int.Parse(program[r]));
@@ -23,12 +23,12 @@ namespace AOC2024
 
         public override string PartOne()
         {
-            return Execute(data.A, data.B, data.C, data.Program, (string x) => { return true; });
+            return Execute(data.A, data.B, data.C, data.Program);
         }
 
 
 
-        string Execute(long A, long B, long C, List<int> Program, Func<string, bool> outCallback)
+        string Execute(long A, long B, long C, List<int> Program)
         {
             StringBuilder result = new();
             int CommandAddress = 0;
@@ -83,7 +83,6 @@ namespace AOC2024
                             else if (value == 6) value = C;
                             if (result.Length > 0) result.Append(',');
                             result.Append(value % 8);
-                            if (!outCallback(result.ToString())) return string.Empty;
                             break;
                         }
                     case 6:
@@ -112,24 +111,46 @@ namespace AOC2024
         public override string PartTwo()
         {
             long result = 0;
-            while (result < long.MaxValue)
-            {
-                if (Execute(result, data.B, data.C, data.Program, CheckOutput) == data.ProgramText)
-                    return result.ToString();
-                ++result;
-            }
+            result = BitsFind();
             return result.ToString();
         }
 
-        bool CheckOutput(string x)
+        long BitsFind()
         {
-            for (int i = 0; i < data.ProgramText.Length && i < x.Length; ++i)
-                if (x[i] != data.ProgramText[i]) return false;
+            for (long i = 0; i < 1024; ++i)
+            {
+                var r = BitsFind(i, 1);
+                if (r > 0)
+                    return r;
+            }
+            return 0;
+        }
+
+        long BitsFind(long result, int lastCount)
+        {
+            if (lastCount > data.Program.Count) return 0;
+            var currentResult = Execute(result, 0, 0, data.Program);
+            if (!CheckOutput(currentResult, lastCount)) return 0;
+            else if (lastCount == data.Program.Count) return result;
+            for (long i = 0; i < 1024; ++i)
+            {
+                var r = BitsFind((result << 3) + i, lastCount + 1);
+                if (r > 0)
+                    return r;
+            }
+            return 0;
+        }
+
+        bool CheckOutput(string x, int lastCount)
+        {
+            if (x.Length > data.ProgramText.Length || x.Length != 2 * lastCount - 1) return false;
+            for (int i = 0; i < x.Length; ++i)
+                if (x[i] != data.ProgramText[data.ProgramText.Length - x.Length + i]) return false;
             return true;
         }
     }
 }
 /*
- 
+
  Подбираем 3 бита, такие, чтобы получилось последнее, не подобранное число, если получилось, то проверяем, со всеми ранее полученными числами
  */
