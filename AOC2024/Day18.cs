1c299ce9e6063c34c396577b6c8b919a4d3e2dd4
Gribovod <gribovod1@gmail.com>
2024-12-18 10:44:04 +0300

    2024 Day 18 solved

---
+++ AOC2024/Day18.cs
@@ -0,0 +1,73 @@
+﻿using AnyThings;
+using Coord = (int x, int y);
+
+namespace AOC2024
+{
+    internal class Day18 : DayPattern<(Coord size, HashSet<Coord> bytes, List<Coord> AllBytes)>
+    {
+        public override void Parse(string singleText)
+        {
+            Coord END_COORDINATES = (70, 70);
+            int START_COUNT = 1024;
+
+            data = new();
+            data.bytes = new();
+            data.AllBytes = new();
+            var t = singleText.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            for (int r = 0; r < t.Length; r++)
+            {
+                var c = t[r].Split(',');
+                data.AllBytes.Add((int.Parse(c[0]), int.Parse(c[1])));
+            }
+            for (int r = 0; r < START_COUNT; r++)
+                data.bytes.Add(data.AllBytes[r]);
+            data.size = END_COORDINATES;
+        }
+
+        public override string PartOne()
+        {
+            return ByteTest().ToString();
+        }
+
+        public override string PartTwo()
+        {
+            int current = data.bytes.Count - 1;
+            do
+            {
+                ++current;
+                data.bytes.Add(data.AllBytes[current]);
+            }
+            while (ByteTest() != 0);
+            return $"{data.AllBytes[current].x},{data.AllBytes[current].y}";
+        }
+
+        int ByteTest()
+        {
+            Dictionary<Coord, int> sand = new();
+            sand.Add(data.size, 0);
+            bool modified = true;
+            while (!sand.ContainsKey((0, 0)) && modified)
+            {
+                modified = false;
+                var ss = sand.ToList();
+                foreach (var c in ss)
+                {
+                    if (AddNext(sand, (c.Key.x + 1, c.Key.y))) modified = true;
+                    if (AddNext(sand, (c.Key.x - 1, c.Key.y))) modified = true;
+                    if (AddNext(sand, (c.Key.x, c.Key.y + 1))) modified = true;
+                    if (AddNext(sand, (c.Key.x, c.Key.y - 1))) modified = true;
+                    sand[c.Key]++;
+                }
+            }
+            return modified ? sand[(data.size.x, data.size.y)] : 0;
+        }
+
+        bool AddNext(Dictionary<Coord, int> sand, Coord next)
+        {
+            if (sand.ContainsKey(next) || data.bytes.Contains(next) || next.x < 0 || next.y < 0 || next.x > data.size.x || next.y > data.size.y)
+                return false;
+            sand.Add(next, 0);
+            return true;
+        }
+    }
+}
--- AOC2024/Program.cs
+++ AOC2024/Program.cs
@@ -1,4 +1,4 @@
-﻿using CurrentDay = AOC2024.Day16;
+﻿using CurrentDay = AOC2024.Day18;
 
 var currentDay = new CurrentDay();
 currentDay.Exec($"Data/{currentDay.GetType().Name}.txt");
