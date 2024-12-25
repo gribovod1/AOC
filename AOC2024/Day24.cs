1b7a1d2769f91c4729b4ffcc5920cc8969fe90d0
Gribovod <gribovod1@gmail.com>
2024-12-25 16:59:51 +0300

    2024/25 solved

--- AOC2024/Day24.cs
+++ AOC2024/Day24.cs
@@ -1,40 +1,70 @@
 ﻿using AnyThings;
-using System.Text;
-using System.Xml.Linq;
 
 namespace AOC2024
 {
-    interface Element
+    abstract class Element
     {
-        string Name { get; }
+        public string Name { get; set; }
 
-        int? Value { get; }
-        abstract bool Process();
+        public override string ToString()
+        {
+            return Name;
+        }
 
-        abstract bool ProcessAll();
+        public int? Value { get; set; }
 
-        bool isZ()
+        public abstract bool Process();
+
+        public abstract bool ProcessAll();
+
+        public bool isZ()
         {
             return (this is Wire) && Name[0] == 'z';
         }
 
-        int GetIndex()
+        public bool isX()
+        {
+            return (this is Wire) && Name[0] == 'x';
+        }
+
+        public bool isY()
+        {
+            return (this is Wire) && Name[0] == 'y';
+        }
+
+        public int GetIndex()
         {
             int index = 0;
             while (char.IsLetter(Name[index])) ++index;
             return int.Parse(Name.Substring(index));
         }
+
+        public void Clear()
+        {
+            Value = null;
+        }
+
+        public abstract List<Element> GetInputs();
+
+        public List<Element> GetParents(List<Element>? prev = null)
+        {
+            var result = prev ?? new List<Element>();
+            var inputs = GetInputs();
+            foreach (var input in inputs)
+                if (input != null)
+                {
+                    result.Add(input);
+                    input.GetParents(result);
+                }
+            return result;
+        }
     }
 
     class Wire : Element
     {
-        public string Name { get; set; }
-
-        public int? Value { get; set; }
-
         public Element? InputElement;
 
-        public bool Process()
+        public override bool Process()
         {
             if (Value != null) return true;
             if (InputElement == null || InputElement.Value == null) return false;
@@ -42,11 +72,16 @@ namespace AOC2024
             return true;
         }
 
-        public bool ProcessAll()
+        public override bool ProcessAll()
         {
             if (Process()) return true;
             if (InputElement != null) InputElement.ProcessAll();
             return Process();
+        }
+
+        public override List<Element> GetInputs()
+        {
+            return new List<Element>() { InputElement };
         }
 
         public Wire(string name, int? value = null)
@@ -58,14 +93,10 @@ namespace AOC2024
 
     class GateAND : Element
     {
-        public string Name { get; set; }
-
-        public int? Value { get; set; }
-
         Element source1;
         Element source2;
 
-        public bool Process()
+        public override bool Process()
         {
             if (Value != null) return true;
             if (source1.Value == null || source2.Value == null) return false;
@@ -73,12 +104,17 @@ namespace AOC2024
             return true;
         }
 
-        public bool ProcessAll()
+        public override bool ProcessAll()
         {
             if (Process()) return true;
             source1.ProcessAll();
             source2.ProcessAll();
             return Process();
+        }
+
+        public override List<Element> GetInputs()
+        {
+            return new List<Element>() { source1,source2 };
         }
 
         public GateAND(string name, Element source1, Element source2, int? value = null)
@@ -92,14 +128,10 @@ namespace AOC2024
 
     class GateOR : Element
     {
-        public string Name { get; set; }
-
-        public int? Value { get; set; }
-
         Element source1;
         Element source2;
 
-        public bool Process()
+        public override bool Process()
         {
             if (Value != null) return true;
             if (source1.Value == null || source2.Value == null) return false;
@@ -107,12 +139,17 @@ namespace AOC2024
             return true;
         }
 
-        public bool ProcessAll()
+        public override bool ProcessAll()
         {
             if (Process()) return true;
             source1.ProcessAll();
             source2.ProcessAll();
             return Process();
+        }
+
+        public override List<Element> GetInputs()
+        {
+            return new List<Element>() { source1, source2 };
         }
 
         public GateOR(string name, Element source1, Element source2, int? value = null)
@@ -126,14 +163,10 @@ namespace AOC2024
 
     class GateXOR : Element
     {
-        public string Name { get; set; }
-
-        public int? Value { get; set; }
-
         Element source1;
         Element source2;
 
-        public bool Process()
+        public override bool Process()
         {
             if (Value != null) return true;
             if (source1.Value == null || source2.Value == null) return false;
@@ -141,12 +174,17 @@ namespace AOC2024
             return true;
         }
 
-        public bool ProcessAll()
+        public override bool ProcessAll()
         {
             if (Process()) return true;
             source1.ProcessAll();
             source2.ProcessAll();
             return Process();
+        }
+
+        public override List<Element> GetInputs()
+        {
+            return new List<Element>() { source1, source2 };
         }
 
         public GateXOR(string name, Element source1, Element source2, int? value = null)
@@ -204,7 +242,8 @@ namespace AOC2024
                     if (g.Name[0] == 'x')
                     {
                         xValue |= ((long)g.Value.Value << index);
-                    } else
+                    }
+                    else
                     {
                         yValue |= ((long)g.Value.Value << index);
                     }
@@ -229,16 +268,6 @@ namespace AOC2024
                         resultInt |= ((long)element.Value.Value << index);
                     }
                 }
-                /*  if (!element.Process())
-                  { q.Enqueue(element); }
-                  else
-                  {
-                      if (element.isZ())
-                      {
-                          int index = element.GetIndex();
-                          result |= (element.Value.Value << index);
-                      }
-                  }*/
             }
             return resultInt.ToString("B");
 
@@ -246,7 +275,29 @@ namespace AOC2024
 
         long GetDifference(long x, long y)
         {
-
+            var trueValue = x + y;
+            foreach (Element element in data.Values)
+            {
+                if (element.isX()) { element.Value = (int)((x >> element.GetIndex()) & 1); }
+                else if (element.isY()) { element.Value = (int)((y >> element.GetIndex()) & 1); }
+                else { element.Clear(); }
+            }
+            long falseValue = 0;
+            var list = data.Values.ToList();
+            Queue<Element> q = new Queue<Element>(list);
+            while (q.Count > 0)
+            {
+                Element element = q.Dequeue();
+                if (element.isZ())
+                {
+                    if (element.ProcessAll())
+                    {
+                        int index = element.GetIndex();
+                        falseValue |= ((long)element.Value.Value << index);
+                    }
+                }
+            }
+            return trueValue ^ falseValue;
         }
 
         public override string PartTwo()
@@ -261,7 +312,26 @@ namespace AOC2024
             6. После проверки каждого входного бита x с каждым входным битом y, получим провода, выставленные не верно
             7. Если таких проводов 8 - то ответ найден
              */
+            HashSet<Element> suspects = new();
+            long wrong_bits = 0;
+            foreach (Element element in data.Values)
+                if (element is Wire) suspects.Add(element);
+            for (int x = 0; x < 45; ++x)
+                for (int y = 0; y < 45; ++y)
+                {
+                    var diff = GetDifference(((long)1 << x), ((long)1 << y));
+                    wrong_bits |= diff;
+                    //for(int i =0; i < 45;++i)
+                    //HashSet<Element> GetParent(data[]);
+                }
 
+            for (int w = 0; w <= 45; ++w)
+          //      if ((int)((wrong_bits >> w) & 1) == 1)
+                {
+                    var n = "z" + w.ToString("00");
+                    var e = data[n];
+                    List<Element> parents = e.GetParents();
+                }
 
 
             return result.ToString();
---
+++ AOC2024/Day25.cs
@@ -0,0 +1,54 @@
+﻿using AnyThings;
+using System.Text;
+using System.Xml.Linq;
+
+namespace AOC2024
+{
+    internal class Day25 : DayPattern<(List<int> locks, List<int> keys)>
+    {
+        public override void Parse(string singleText)
+        {
+            var LocksAndKeys = singleText.Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            data = new();
+            data.locks = new();
+            data.keys = new();
+            foreach (var s in LocksAndKeys)
+            {
+                var lines = s.Split(Environment.NewLine);
+                int value = 0;
+                for (var r = 1; r < lines.Length - 1; r++)
+                    for (var c = 0; c < lines[r].Length; c++)
+                        if (lines[r][c] == '#')
+                            value += (1 << c * 4);
+                if (lines[0] == "#####")
+                    data.locks.Add(value);
+                else
+                    data.keys.Add(value);
+            }
+        }
+
+        /* Выделим каждому столбцу по 4 бита в числе и при сложении в каждой тетраде не должно быть числа больше 5 */
+        public override string PartOne()
+        {
+            long result = 0;
+            for (int l = 0; l < data.locks.Count; ++l)
+                for (int k = 0; k < data.keys.Count; ++k)
+                    if (check(data.locks[l] + data.keys[k]))
+                        ++result;
+            return result.ToString();
+        }
+
+        bool check(int value)
+        {
+            for (int i = 0; i < 5; ++i)
+                if (((value >> i * 4) & 0b1111) > 5) return false;
+            return true;
+        }
+
+        public override string PartTwo()
+        {
+            long result = 0;
+            return result.ToString();
+        }
+    }
+}
